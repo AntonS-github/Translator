2ec969172e19ad400ed40a1dab5351a91e2d10ea
AntonS <surkov.ved@gmail.com>
2022-07-25 16:08:03 +0300

    All Done

--- main.py
+++ main.py
@@ -4,20 +4,17 @@ import pdfplumber
 import os
 from reportlab.lib.pagesizes import LETTER
 from reportlab.pdfgen.canvas import Canvas
-from gtts import gTTS
-
 
 def pdf_translator(src_path):
     if pathlib.Path(src_path).is_file and pathlib.Path(src_path).suffix == '.pdf':
         print(f'Source file is {pathlib.Path(src_path).name}')
-        print('Loading...')
+
         with pdfplumber.PDF(open(file=src_path, mode='rb')) as src_pdf:
             pages = [page.extract_text() for page in src_pdf.pages]
         src_text = ''.join(pages)
-        print(src_text)
         translator = Translator()
         res_text = translator.translate(src_text)
-        print(res_text.text)
+
         os.chdir('res_path')
         canvas = Canvas('result.pdf', pagesize=LETTER)
         canvas.setPageSize((400, 400))
@@ -25,10 +22,12 @@ def pdf_translator(src_path):
         canvas.drawString(10, 380, res_text.text)
         canvas.save()
 
+        return print(f'Исходный текст: {src_text}\nИсходный язык: {res_text.src}\nЯзык результата: {res_text.dest}\n'
+              f'Текст результата:  {res_text.text}\n'
+              f'Файл с результатом в формате pdf: "./res_path/result.pdf"')
 
 def main():
     pdf_translator('C:\\Users\\anvla\\Desktop\\Учёба\\py\\Translator\\src_files\\Test.pdf')
-
 
 if __name__ == '__main__':
     main()
